7652b4eed7946f51e3e09e6166d547452690f9a8
Nicholas Mandelbaum <nicholas@wehavesun.com>
2020-10-05 12:35:16 +0100

    fix link

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,17 @@ export default function Home() {
     <h1>On se perd parfois</h1>
       <h2>Nicholas Mandelbum</h2>
       <ul>
-    <li><Link href='/lire/chapitre-1'><a>Chapitre 1</a></Link></li>
-    <li><Link href='/lire/chapitre-2'><a>Chapitre 2</a></Link></li>
-    <li><Link href='/lire/chapitre-3'><a>Chapitre 3</a></Link></li>
-    <li><Link href='/lire/chapitre-4'><a>Chapitre 4</a></Link></li>
-    <li><Link href='/lire/chapitre-5'><a>Chapitre 5</a></Link></li>
-    <li><Link href='/lire/chapitre-6'><a>Chapitre 6</a></Link></li>
-    <li><Link href='/lire/chapitre-7'><a>Chapitre 7</a></Link></li>
-    <li><Link href='/lire/chapitre-8'><a>Chapitre 8</a></Link></li>
-    <li><Link href='/lire/chapitre-9'><a>Chapitre 9</a></Link></li>
+        <li><Link href='/lire/chapitre-01'><a>Chapitre 1</a></Link></li>
+        <li><Link href='/lire/chapitre-02'><a>Chapitre 2</a></Link></li>
+        <li><Link href='/lire/chapitre-03'><a>Chapitre 3</a></Link></li>
+        <li><Link href='/lire/chapitre-04'><a>Chapitre 4</a></Link></li>
+        <li><Link href='/lire/chapitre-05'><a>Chapitre 5</a></Link></li>
+        <li><Link href='/lire/chapitre-06'><a>Chapitre 6</a></Link></li>
+        <li><Link href='/lire/chapitre-07'><a>Chapitre 7</a></Link></li>
+        <li><Link href='/lire/chapitre-08'><a>Chapitre 8</a></Link></li>
+        <li><Link href='/lire/chapitre-09'><a>Chapitre 9</a></Link></li>
         <li><Link href='/lire/chapitre-10'><a>Chapitre 10</a></Link></li>
-        </ul>
+      </ul>
     </>
   )
 }
