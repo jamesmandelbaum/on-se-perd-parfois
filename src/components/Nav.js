b7392ea424234277ca635441bf5d9b864d754154
Nicholas Mandelbaum <nicholas@wehavesun.com>
2020-10-11 11:23:51 +0100

    change navigation system

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,29 +3,35 @@ import { useRouter } from 'next/router'
 import styles from '../../styles/Nav.module.scss'
 
 
-const chapters = ['01','02','03','04','05','06','07','08','09','10']
+const chapters = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']
+
 
 export const Header = () => {
-const {pathname} = useRouter()
+const { pathname } = useRouter()
 return( 
-      <ul className={styles.ul}>
+  <ul className={styles.ul}>
         <li><Link href='/'><a>←</a></Link></li>
       {chapters.map(
         (chapter) => 
-          <li key={chapter}><Link href={`/lire/chapitre-${chapter}`}>
-          <a className={pathname === `/lire/chapitre-${chapter}` && styles.active}>{chapter}</a>
-          
+        <li key={chapter}><Link href={`/lire/chapitre-${chapter}`}>
+          <a className={pathname === `/lire/chapitre-${chapter}` ? styles.active : undefined}>{chapter}</a>       
           </Link>
           </li >
       )}
       </ul>
-    
-  )
-}
-
+    )
+  }
+  
 export const Footer = () => {
-  return(
-    <br />
+  const { pathname } = useRouter()
+  const index = pathname.slice(-2);
+  const next = chapters.indexOf(index) + 1
+  const linkToNext = chapters[next];
+  return (
+    <div className={styles.footer}>
+      { linkToNext && <Link href={`/lire/chapitre-${linkToNext}`}><a>→ Chapitre suivant</a></Link>}
+    </div>
   )
+
 }
 
